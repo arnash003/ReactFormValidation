6d48dc00374eb336e4fff37572b2530048dfd524
Asher-jay Arenz <asher-jayarenz@Asher-jays-MacBook-Air.local>
2022-04-30 19:43:55 +0900

    React Form Validation

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+const initialState = {
+  name: "",
+    email: "",
+    password: "",
+    nameError: "",
+    emailError: "",
+    passwordError: ""
+}
+
+export default class ValidationForm extends React.Component {
+  state = initialState;
+
+  handleChange = event => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+      ? event.target.checked
+      : event.target.value
+    });
+  };
+
+  validate = () => {
+    // let nameError= "";
+    let emailError= "";
+    // let passwordError= "";
+
+    if (!this.state.email.includes('@')) {
+      emailError = 'invalid email';
+    }
+
+    if (emailError) {
+      this.setState({emailError});
+      return false;
+    }
+    return true;
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    if (isValid) {
+    console.log(this.state);
+    }
+    // clear the form
+    this.setState(initialState);
+  };
+
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <input
+          name="name"
+          placeholder="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+          />
+        
+         <div style={{ fontSize: 12, color: 'red'}}>
+         {this.state.nameError}
+         </div>
+    
+          </div>
+          <div>
+            <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.handleChange} 
+            />
+             <div style={{ fontSize: 12, color: 'red'}}>
+         {this.state.passwordError}
+         </div>
+            </div>
+            <div>
+              <input
+              name="email"
+              placeholder="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              />
+               <div style={{ fontSize: 12, color: 'red'}}>
+         {this.state.emailError}
+         </div>
+              </div>
+            <button type="submit">Submit</button>
+            </form>
+    )
+  }
+}
+
+// Validating the form
+// When the user submits the form I want to validate and show any errors that exist
